47d5eb05176ec758003b366313c35b16d45e758d
root <root@ns4007311.ip-198-27-83.net>
2013-06-07 18:54:02 -0400

    weee instead of wee ;-)

--- lineCount5.pl
+++ lineCount5.pl
@@ -56,7 +56,7 @@ if ($wday == 0) { $Wday = 'Sun'; }
 my $today = $mday." ".$Month." ".($year+1900)." ".$Wday."\n";  
 my $weekend = 'weekday';  
 if ((($Wday eq 'Sat') || ($Wday eq 'Sun')) || (($Wday eq 'Fri') && (($hour eq '18') || ($hour eq '19') || ($hour eq '20') || ($hour eq '21') || ($hour eq '22') || ($hour eq '23'))))		{
-	$weekend = 'weeekend';
+	$weekend = 'weekend';
 }
 
 open (Fd, "<$datefile");
